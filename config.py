0ed7c5ae771fef4b609b803ea07453114309c15a
David Parker <davidparker333@gmail.com>
2022-06-20 13:39:31 -0400

    fix

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # dev only
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
 
 class Config():
